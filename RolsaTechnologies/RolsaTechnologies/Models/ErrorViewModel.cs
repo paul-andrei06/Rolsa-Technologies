0d0c361aa8eea50097697e27c0cc5fe44be48656
paul-andrei06 <148383402+paul-andrei06@users.noreply.github.com>
2025-03-19 12:14:08 +0000

    Day 1
    Just Created a blank asp.net mvc application and uploaded it to GitHub

---
+++ RolsaTechnologies/RolsaTechnologies/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RolsaTechnologies.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
